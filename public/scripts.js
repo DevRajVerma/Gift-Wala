769b221cc33aa1ceca34def4353e3ef03436e457
Dev <drverma2704@gmail.com>
2023-08-14 13:29:34 +0530

    Added Some extra pages

---
+++ public/scripts.js
@@ -0,0 +1,43 @@
+// Sample product data (replace with your actual data)
+const products = [
+    { id: 1, name: 'Product 1', price: 19.99 },
+    { id: 2, name: 'Product 2', price: 24.99 },
+    // Add more products
+];
+
+// Cart data
+const cart = [];
+
+// Function to add product to cart
+function addToCart(productId) {
+    const product = products.find(item => item.id === productId);
+    if (product) {
+        cart.push(product);
+        updateCart();
+    }
+}
+
+// Function to update the cart display
+function updateCart() {
+    const cartItems = document.getElementById('cartItems');
+    const cartTotal = document.getElementById('cartTotal');
+    
+    cartItems.innerHTML = cart.map(item => `<li>${item.name} - $${item.price}</li>`).join('');
+    
+    const totalAmount = cart.reduce((total, item) => total + item.price, 0).toFixed(2);
+    cartTotal.textContent = `$${totalAmount}`;
+}
+
+// Dynamically generate product cards
+const productCards = document.getElementById('products');
+productCards.innerHTML = products.map(product =>
+    `<div class="product-card">
+        <h3>${product.name}</h3>
+        <p>$${product.price}</p>
+        <button onclick="addToCart(${product.id})">Add to Cart</button>
+    </div>`
+).join('');
+
+
+// ... (previous code) ...
+
--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const port = 3000; // Change this as needed
 
 // Serve static files (HTML, CSS, images)
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// Sample product data
+const products = [
+    { id: 1, name: 'Product 1', price: 19.99, description: 'Description for Product 1' },
+    { id: 2, name: 'Product 2', price: 24.99, description: 'Description for Product 2' },
+    // Add more products
+];
+
+// Route to display product details page
+app.get('/product/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const product = products.find(item => item.id === productId);
+    if (product) {
+        res.render('product', { product });
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
 
 // Example API endpoint to simulate fetching products
 app.get('/api/products', (req, res) => {
