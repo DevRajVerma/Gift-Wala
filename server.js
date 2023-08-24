c63ff0d002f8ceec43f9396443e2c92ee23271a8
Dev Raj Verma <27505devraj.2021cse@gmail.com>
2023-08-24 19:29:18 +0530

    Add to products form and Products page added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const app = express();
 
 const UserModel = require("./model/user.js");
+const Product = require('./model/product.js');
 
 const port = 3000; // Change this as needed
 
@@ -101,9 +102,7 @@ app.get("/cart",(req,res) => {
   res.render("cart");
 })
 
-app.get("/add_products",(req,res) => {
-  res.render("add_products");
-})
+
 
 app.get("/contact",(req,res) => {
 
@@ -122,6 +121,27 @@ app.get("/api/contact",(req,res) => {
   login_status: login_status});
   
 })
+
+app.get("/api/add_products",(req,res) => {
+
+  var title = " ";
+  var login_status = "";
+  res.render("add_products", {name:title,
+  login_status: login_status});
+  
+})
+
+app.get("/add_products",(req,res) => {
+
+  var title = " ";
+  var login_status = "";
+  res.render("add_products", {name:title,
+  login_status: login_status});
+  
+})
+
+
+
 
 app.get("/api/profile",(req,res) => {
 
@@ -148,9 +168,21 @@ app.get("/checkout",(req,res) => {
 
 app.post("/api/add_products", async (req, res) =>{
 
-  const { productName, Description, Price } = req.body;
-  console.log(req.body);
-  res.redirect("/");
+  const { productName, Description, Price, image } = req.body;
+  try{ 
+    const product = await Product.findOne({ProductName:productName});
+    if(product){
+      return res.status(201).json({message:"Product already Exist"})
+    }
+
+     const createProduct = new Product({ProductName:productName,Desription:Description,Price:Price,ImageUrl:image});
+     await createProduct.save();
+     res.status(201).json({Messgae:createProduct});
+    //  res.redirect("/");
+  }catch(err){
+    console.log("Error in creating the Product",err);
+    res.status(404).json({Messgae:"Error in creating Product"});
+  }
 })
 
 app.post("/api/signup", async (req, res) => {
@@ -192,6 +224,28 @@ app.get("/api/login",  (req, res)=> {
 });
 
 
+app.get("/products",  (req, res)=> {
+
+  var login_status = "";
+    var title = "";
+
+    var ProductName = "Ankit";
+    var ImageUrl ="/images/Ankit.jpg";
+    var Price = "500";
+    var Description="Very lamba land";
+
+    res.render("products", {name:title,
+      login_status: login_status, ProductName: ProductName,
+      ImageUrl : ImageUrl,
+      Price : Price,
+      Description : Description
+
+
+
+     });
+
+  // res.render("login");
+});
 
 app.get("/api/signup", function (req, res) {
   res.render("signup");
